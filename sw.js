775dfed1f15762bee6452fbefcf9c3b749dacd19
mutwkilboessa <mutwkil.a.h.m@gmail.com>
2026-05-28 08:19:47 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='sanad-v18';
+const BASE='/Sanad';
+self.addEventListener('install',function(e){e.waitUntil(caches.keys().then(function(ks){return Promise.all(ks.map(function(k){return caches.delete(k);}));}).then(function(){return caches.open(CACHE).then(function(c){return c.addAll([BASE+'/index.html',BASE+'/manifest.json',BASE+'/icon-192.png',BASE+'/icon-512.png']);});}).then(function(){return self.skipWaiting();}));});
+self.addEventListener('activate',function(e){e.waitUntil(self.clients.claim());});
+self.addEventListener('fetch',function(e){e.respondWith(caches.match(e.request).then(function(r){return r||fetch(e.request);}));});
